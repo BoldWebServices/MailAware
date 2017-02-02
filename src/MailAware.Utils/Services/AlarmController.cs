febb0b8f64dd5fe2509fea670729739f0329a4eb
Stephen Daudell <s.daudell@boldwebservices.com>
2017-02-01 23:42:34 -0600

    Began implementation of alarm state controller.

---
+++ src/MailAware.Utils/Services/AlarmController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MailAware.Utils.Services
+{
+    /// <summary>
+    /// Implementation of an alarm state controller.
+    /// </summary>
+    public class AlarmController : IAlarmController
+    {
+        #region Constants and Enums
+
+        private enum AlarmState
+        {
+            Uninitialized,
+            Normal,
+            AlarmThresholdExceeded
+        }
+
+        #endregion
+
+        public AlarmController()
+        {
+            _currentState = AlarmState.Uninitialized;
+            _lastMessageReceivedDate = DateTime.MinValue;
+        }
+
+        /// <see cref="IAlarmController.Initialize" />
+        public void Initialize(int alarmThresholdSecs)
+        {
+            _alarmThresholdSecs = alarmThresholdSecs;
+            _currentState = AlarmState.Normal;
+            _lastMessageReceivedDate = DateTime.Now;
+        }
+
+        /// <see cref="IAlarmController.MessageSeen" />
+        public void MessageSeen(DateTime date)
+        {
+            if (_currentState == AlarmState.Uninitialized)
+            {
+                throw new InvalidOperationException("Alarm controller not initialized.");
+            }
+        }
+
+        /// <see cref="IAlarmController.MessageSeen" />
+        public async Task ProcessState()
+        {
+            if (_currentState == AlarmState.Uninitialized)
+            {
+                throw new InvalidOperationException("Alarm controller not initialized.");
+            }
+        }
+
+        #region Fields
+
+        private AlarmState _currentState;
+        private DateTime _lastMessageReceivedDate;
+        private int _alarmThresholdSecs;
+
+        #endregion
+    }
+}
---
+++ src/MailAware.Utils/Services/IAlarmController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MailAware.Utils.Services
+{
+    /// <summary>
+    /// Interface for an alarm state controller.
+    /// </summary>
+    public interface IAlarmController
+    {
+        /// <summary>
+        /// Sets up the alarm controller to be in a good non-alarmed state.
+        /// </summary>
+        /// <param name="alarmThresholdSecs">The threshold in seconds for signalling an alarm state.</param>
+        void Initialize(int alarmThresholdSecs);
+
+        /// <summary>
+        /// Processes a message being received at a particular date.
+        /// </summary>
+        /// <param name="date">The date and time that the message was received.</param>
+        void MessageSeen(DateTime date);
+
+        /// <summary>
+        /// Handles alarm state management, sending notifications as necessary.
+        /// </summary>
+        Task ProcessState();
+    }
+}
